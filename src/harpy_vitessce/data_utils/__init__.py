c2a872e82192833b371666c7dc104bfd236e5b4a
ArneDefauw <arnedefauw@gmail.com>
2026-02-11 11:18:44 +0100

    safe cast

--- src/harpy_vitessce/data_utils/__init__.py
+++ src/harpy_vitessce/data_utils/__init__.py
@@ -4,6 +4,7 @@ from ._ome import xarray_to_ome_zarr, array_to_ome_zarr
 from ._adata import (
     copy_annotations,
     downcast_int64_to_int32,
+    safe_cast_integer_like_float32,
 )
 from ._visium_hd import example_visium_hd_processing
 
@@ -13,4 +14,5 @@ __all__ = [
     "copy_annotations",
     "example_visium_hd_processing",
     "downcast_int64_to_int32",
+    "safe_cast_integer_like_float32",
 ]
