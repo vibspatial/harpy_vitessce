9b79dc2f1bd5356c1e21022fb7ed3f8f22a16e78
ArneDefauw <arnedefauw@gmail.com>
2026-05-21 11:01:56 +0200

    visium

--- scripts/benchmark_scripts/visium.sh
+++ scripts/benchmark_scripts/visium.sh
@@ -5,56 +5,30 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 S3=false
 
-PLATFORMS_TO_RUN=("Visium_HD")
-# Examples:
-#PLATFORMS_TO_RUN=("Visium")
-# PLATFORMS_TO_RUN=("Visium_HD")
+PLATFORMS_TO_RUN=("Visium")
 
 EXPERIMENT_NAMES=(
-  "SCA001"
-  "SCA002"
-  "SCA003"
-  "SCA010"
-  "SCA017"
-  "SCA018"
-  "SCA019"
-  "SCA020"
-  "SCA021"
-  "SCA023"
-  "SCA024"
+  "SCA013"
+  "SCA014"
+  "SCA015"
+  "SCA016"
 )
 
 declare -A MICRONS_PER_PIXEL_BY_EXPERIMENT=(
-  ["SCA001"]="0.2196490235773455"
-  ["SCA002"]="0.21949344319707406"
-  ["SCA003"]="0.4425852423681751"
-  ["SCA010"]="0.4421652041595308"
-  ["SCA017"]="0.44220784830557425"
-  ["SCA018"]="0.4422736954685126"
-  ["SCA019"]="0.4432412469726364"
-  ["SCA020"]="0.4422816200157306"
-  ["SCA021"]="0.44208963838418674"
-  ["SCA023"]="0.442"
-  ["SCA024"]="0.442"
+  ["SCA013"]="0.442"
+  ["SCA014"]="0.442"
+  ["SCA015"]="0.442"
+  ["SCA016"]="0.442"
 )
 
 declare -A PLATFORM_BY_EXPERIMENT=(
-  ["SCA001"]="Visium_HD"
-  ["SCA002"]="Visium_HD"
-  ["SCA003"]="Visium_HD"
-  ["SCA010"]="Visium_HD"
-  ["SCA017"]="Visium_HD"
-  ["SCA018"]="Visium_HD"
-  ["SCA019"]="Visium_HD"
-  ["SCA020"]="Visium_HD"
-  ["SCA021"]="Visium_HD"
-  ["SCA023"]="Visium_HD"
-  ["SCA024"]="Visium_HD"
+  ["SCA013"]="Visium"
+  ["SCA014"]="Visium"
+  ["SCA015"]="Visium"
+  ["SCA016"]="Visium"
 )
 
-RESOLUTIONS=("02" "08" "16" "20" "120")
-
-RESOLUTIONS=( "20")
+RESOLUTIONS=("120")
 
 for PLATFORM in "${PLATFORMS_TO_RUN[@]}"; do
   for EXPERIMENT_NAME in "${EXPERIMENT_NAMES[@]}"; do
@@ -88,10 +62,6 @@ for PLATFORM in "${PLATFORMS_TO_RUN[@]}"; do
 
     # conversion
     for RESOLUTION in "${RESOLUTIONS[@]}"; do
-      if [[ "${PLATFORM}" == "Visium" && "${RESOLUTION}" != "20" && "${RESOLUTION}" != "120" ]]; then
-        continue
-      fi
-
       OUTPUT_DIR="${OUTPUT_BASE_DIR}/${RESOLUTION}um"
       OUTPUT_PATH_IMG="${OUTPUT_DIR}/image.ome.zarr"
       OUTPUT_PATH_ADATA="${OUTPUT_DIR}/adata_${RESOLUTION}um.zarr"
@@ -116,10 +86,6 @@ for PLATFORM in "${PLATFORMS_TO_RUN[@]}"; do
 
     # create config
     for RESOLUTION in "${RESOLUTIONS[@]}"; do
-      if [[ "${PLATFORM}" == "Visium" && "${RESOLUTION}" != "20" && "${RESOLUTION}" != "120" ]]; then
-        continue
-      fi
-
       OUTPUT_DIR="${OUTPUT_BASE_DIR}/${RESOLUTION}um"
       OUTPUT_PATH_IMG="${OUTPUT_DIR}/image.ome.zarr"
       OUTPUT_PATH_ADATA="${OUTPUT_DIR}/adata_${RESOLUTION}um.zarr"
